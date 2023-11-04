c95e046f0ebfc73dab7e13abc8ead567312fdd02
Kiran Anand <liteninkiran@hotmail.com>
2023-11-04 18:27:17 +0000

    Add material module

--- resources/client/src/app/app.module.ts
+++ resources/client/src/app/app.module.ts
@@ -1,18 +1,27 @@
+// Angular
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// Modules
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './modules/material.module';
 
 @NgModule({
     declarations: [
         AppComponent,
     ],
     imports: [
+        // Core Modules
         BrowserModule,
-        AppRoutingModule,
         BrowserAnimationsModule,
+
+        // Routing
+        AppRoutingModule,
+
+        // Shared Modules
+        MaterialModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
